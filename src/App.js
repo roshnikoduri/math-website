c43ac82c7297f69db35c43fc5f27979bd9539ae2
roshnikoduri <roshnikoduri@gmail.com>
2024-05-19 20:01:21 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,76 @@
-import React from 'react';
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
+import axios from 'axios';
+import Loading from './Loading'
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello, It's my first React App!</h1>
-       <h2>Happy Coding!!</h2>
-    </div>
-  );
-};
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      users: [], 
+      loading:  false
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  getUsers(){
+    this.setState({
+      loading: true
+    })
+
+    axios('https://api.randomuser.me/?nat=US&results=5').then(response=>
+      this.setState({
+      users: [...this.state.users, ...response.data.results],
+      loading: false
+    })
+    );
+  }
+  //state
+  
+  componentWillMount(){
+
+    this.getUsers();
+
+    //make some API calls; before the component mounts
+    //how to actually check console?
+    //add react developer tools
+  }
+
+  handleSubmit = e =>{
+    e.preventDefault();
+    this.getUsers();
+    console.log('more users loaded');
+  }
+
+
+  render(){
+    const {loading, users} = this.state
+    return(
+      <div className="App"> 
+      <form onSubmit = {this.handleSubmit}>
+          <input type = "submit" value = "load users" />
+      </form>
+      <hr /> 
+        {
+          !loading ? (
+            users.map(user => (
+              <div key={user.id.value}>
+                <h3 style={{ color: 'red' }}>{user.name.first}</h3>
+                <p>{user.email}</p>
+                <hr />
+                
+              </div>
+            ))
+          ) : <Loading message =  "hey hey hey" />
+        }
+      </div>
+    );
+  }
+}
+
+//props = properties
+//<hr> = horizontal line
 
 export default App;
---
+++ src/Loading.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Loading = (props) => <h2>{props.message}</h2>
+
+export default Loading;
+//if multiple put export befor const and delete line 5
+//more than 1 consst: put loading in crlybrackets in app.js
